7ab44279b496711cae2d57b6dfd903824ae40188
The Ghostavo <57949820+GustavoBiscaro@users.noreply.github.com>
2025-08-03 14:33:12 -0300

    functions application

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # powerful-pdf
+
+# Férias Programadas
+
+## SQLAlchemy
+
+1. py -m venv venv (criar ambiente virtual)
+
+2. venv\Scripts\activate.bat (para ativar)
+
+3. deactivate para desativar
+
+4. pip install -r .\requirements.txt
+
+5. streamlit run .\menu.py
---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from menu_extrair import exibir_menu_extrair
+from menu_combinar import exibir_menu_combinar
+from menu_marca_dagua import exibir_menu_marca_dagua
+from menu_imagens import exibir_menu_imagens
+
+st.set_page_config(
+    page_title="Powerful PDF",
+    page_icon=":blue_book:",
+    layout="wide"
+    )
+
+_, col2, _ = st.columns(3)
+
+with col2:
+  st.title("Powerful PDF")
+  st.markdown("""
+
+              ### Escolha a opção desejada abaixo:
+
+
+              """)
+
+entradas_menu = {
+  'Extrair página':'file-earmark-pdf-fill',
+  'Combine PDFs':'plus-square-fill',
+  "Adicione marca d'água":'droplet-fill',
+  'Imagem para PDF':'file-earmark-richtext-fill',
+  'Excel para PDF':'file-earmark-spreadsheet-fill',
+}
+
+
+escolha = option_menu(
+    menu_title=None,
+    orientation='horizontal',
+    options=list(entradas_menu.keys()),
+    icons=list(entradas_menu.values()),
+    default_index=0,
+)
+
+_, col2, _ = st.columns(3)
+with col2:
+  match escolha:
+    case 'Extrair página':
+      exibir_menu_extrair(coluna=col2)
+      
+    case 'Combine PDFs':
+      exibir_menu_combinar(coluna=col2)
+
+    case "Adicione marca d'água":
+      exibir_menu_marca_dagua(coluna=col2)
+
+    case 'Imagem para PDF':
+      exibir_menu_imagens(coluna=col2)
+      
+    case _:
+      st.warning('Implementar página')
+
+
+
+    # case 'Excel para PDF':
+    #   st.write("Você escolheu Excel para PDF.")
+    
